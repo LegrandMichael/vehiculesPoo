416f93ea7af1720492a554190442966b207c1103
LegrandMichael <michael.c.legrand@gmail.com>
2017-11-02 10:29:55 +0100

    Index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+ <?php
+    
+    require_once ('Voiture.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Véhicules</title>
+</head>
+<body>
+  
+   <?php $dacia2 = new Voiture("Vert", 12); ?>
+</body>
+</html>
